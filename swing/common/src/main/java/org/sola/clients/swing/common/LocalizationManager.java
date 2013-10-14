bdd65987592db97bac2f63f46b023a2cc2b3b8c3
Bulara <mbulara@gmail.com>
2013-10-14 08:46:06 +0200

    updated sola to version 2.1.1 as at 14 october 2013

--- swing/common/src/main/java/org/sola/clients/swing/common/LocalizationManager.java
+++ swing/common/src/main/java/org/sola/clients/swing/common/LocalizationManager.java
@@ -43,9 +43,9 @@ public class LocalizationManager {
     private static final String LANGUAGE = "language";
     private static final String COUNTRY = "country";
     private static final String WEB_START_HOST_PROP = "SOLA_WEB_START_HOST";
-    private static final String PRODUCTION_HOST_NAME = "gismain";
+    private static final String PRODUCTION_HOST_NAME = "dbmsmain";
     private static final String PRODUCTION_HOST_IP = "192.168.0.10";
-    private static final String SOLA_VERSION = "2.1.0";
+    private static final String SOLA_VERSION = "2.1.1";
 
     /**
      * Loads default language and country codes and sets {@link Locale} settings
