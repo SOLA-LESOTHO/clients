ad5a45debf584ae0dfb830f37d864c28e43966f0
unknown <et.maifo@gmail.com>
2014-02-17 12:32:42 +0200

    Preparing new build of SOLA - v2.1.4

--- swing/common/src/main/java/org/sola/clients/swing/common/LocalizationManager.java
+++ swing/common/src/main/java/org/sola/clients/swing/common/LocalizationManager.java
@@ -45,7 +45,7 @@ public class LocalizationManager {
     private static final String WEB_START_HOST_PROP = "SOLA_WEB_START_HOST";
     private static final String PRODUCTION_HOST_NAME = "gismain";
     private static final String PRODUCTION_HOST_IP = "192.168.0.10";
-    private static final String SOLA_VERSION = "2.1.3";
+    private static final String SOLA_VERSION = "2.1.4";
 
     /**
      * Loads default language and country codes and sets {@link Locale} settings
